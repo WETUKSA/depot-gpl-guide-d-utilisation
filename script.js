bf69e7b048be90fe6dcfbd0ccd0ea8f5b082fcf1
mdiawdev <mamadou.diaw.dev@gmail.com>
2025-08-01 21:42:03 +0200

    change index.html

--- script.js
+++
@@ -1,9 +0,0 @@
-const colorBox = document.getElementById("colorBox");
-const changeColorBtn = document.getElementById("changeColorBtn");
-
-const colors = ["#e74c3c", "#8e44ad", "#f1c40f", "#1abc9c", "#2ecc71", "#e67e22"];
-
-changeColorBtn.addEventListener("click", () => {
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  colorBox.style.backgroundColor = randomColor;
-});
